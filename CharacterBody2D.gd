77920c03aaf73dfad5e597fb438c7cc0d9a116a8
Grupo 4 <sofia.giani@escuelacangallo.edu.ar>
2023-06-07 09:10:26 -0300

    clase 7-6

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -23,6 +23,6 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED) 
 
 	move_and_slide()
