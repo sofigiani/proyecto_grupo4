cb825b1952f908095d6f4eb37025b166d61a428c
Grupo 4 <sofia.giani@escuelacangallo.edu.ar>
2023-06-26 11:25:43 -0300

    avanzamos en el codigo de enemigo

--- enemies/Damageable.gd
+++ enemies/Damageable.gd
@@ -1,10 +1,12 @@
 extends Node
 
+class_name Damageable
+
 @export var health : float = 10 :
 	get:
 		return health
 	set(value):
-		SignalBus.emit_signal(**)
+		SignalBus.emit_signal("on_health_changed", get_parent(), value - health)
 		health = value
 
 func hit(damage : int):
