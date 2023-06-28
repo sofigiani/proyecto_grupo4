60c19a1a72b0892a05f16455eb0cc191d99c094b
Grupo 4 <sofia.giani@escuelacangallo.edu.ar>
2023-06-28 08:14:08 -0300

    anadido el control

--- UI/health_changed_management.gd
+++ UI/health_changed_management.gd
@@ -1,9 +1,9 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	SignalBus.connect("")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
