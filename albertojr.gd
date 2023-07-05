db1b9dc35ab311233f3b0fea8b398eb490f5421f
Grupo 4 <sofia.giani@escuelacangallo.edu.ar>
2023-07-05 09:10:07 -0300

    vamos de nuevo

---
+++ albertojr.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var velocity = Vector2(100,0)
+
+
+func _physics_process(delta):
+	velocity.y += 7
+	
+	if is_on_wall():
+			velocity.x *= -1
+			$shrmp.flip_h = ! $shrmp.flip_h
+			
+	move_and_slide(velocity, Vector2(0, -1))
