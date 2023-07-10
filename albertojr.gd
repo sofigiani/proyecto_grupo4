179b3b5992437b3a4993ae0eeaa719bb327be30b
Grupo 4 <sofia.giani@escuelacangallo.edu.ar>
2023-07-10 11:59:43 -0300

    enemigo listo

--- albertojr.gd
+++ albertojr.gd
@@ -5,9 +5,19 @@ extends CharacterBody2D
 
 #func _physics_process(delta):
 #	velocity.y += 7
-	
-	#if is_on_wall():
-	#		velocity.x *= -1
-	#		$shrmp.flip_h = ! $shrmp.flip_h
-	#		
-	#move_and_slide(velocity, Vector2(0, -1))
+#	
+#	if is_on_wall():
+#			velocity.x *= -1
+#			$shrmp.flip_h = ! $shrmp.flip_h
+			
+#	move_and_slide(velocity, Vector2(0, -1))
+
+
+func _on_top_body_entered(body):
+	if body.get_name() == "Tito":
+		queue_free()
+
+
+func _on_costados_body_entered(body):
+	if body.get_name() == "Tito":
+		body.respawn()
