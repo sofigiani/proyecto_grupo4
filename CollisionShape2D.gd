9731cf34dd461c36b7ff9d884e488e12a7e1d2b0
Grupo 4 <sofia.giani@escuelacangallo.edu.ar>
2023-08-30 08:03:27 -0300

    Tenemos el fondo

---
+++ CollisionShape2D.gd
@@ -0,0 +1,11 @@
+extends CollisionShape2D
+
+
+func _on_top_body_entered(body):
+	if body.get_name() == "Tito":
+		queue_free()
+
+
+func _on_costados_body_entered(body):
+	if body.get_name() == "Tito":
+		body.respawn()
